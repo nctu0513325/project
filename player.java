9449a2ee81c8953e29c590404ba38d19d70d04a9
fumchin <fumchin@gmail.com>
2020-05-19 14:36:55 +0800

    change Voltage to dB, and draw waveform

--- player.java
+++ player.java
@@ -3,18 +3,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-public class player extends Application {
- @Override
- public void start(Stage stage) throws Exception {
- Parent root =
- FXMLLoader.load(getClass().getResource("player.fxml"));
 
- Scene scene = new Scene(root);
- stage.setTitle("player"); // displayed in window's title bar
- stage.setScene(scene);
- stage.show();
- }
- public static void main(String[] args) {
- launch(args);
- }
+public class player extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("player.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setTitle("player"); // displayed in window's title bar
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- playercontroller.java
+++ playercontroller.java
@@ -5,29 +5,50 @@ import javafx.scene.control.Slider;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import javafx.event.ActionEvent;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+
+import java.io.IOException;
+
 import javafx.beans.property.DoubleProperty;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import java.util.*;
 
 public class playercontroller {
 
-    @FXML private Slider slTime;
-    @FXML private Button btnStop;
-    @FXML private Button btnPlay;
-    @FXML private Slider slVolume;
-    @FXML private Label lbVolume;
-    @FXML private Button btnOpen;
-    @FXML private Label lbCurrentTime;
-    @FXML private Slider slSpeed;
-    @FXML private Label lbSpeed;
-    @FXML private MediaView mView;
-    @FXML private Pane pane;
-
+    @FXML
+    private Slider slTime;
+    @FXML
+    private Button btnStop;
+    @FXML
+    private Button btnPlay;
+    @FXML
+    private Slider slVolume;
+    @FXML
+    private Label lbVolume;
+    @FXML
+    private Button btnOpen;
+    @FXML
+    private Label lbCurrentTime;
+    @FXML
+    private Slider slSpeed;
+    @FXML
+    private Label lbSpeed;
+    @FXML
+    private MediaView mView;
+    @FXML
+    private Pane pane;
+    @FXML
+    private Canvas canvas1;
+    @FXML
+    private Canvas canvas2;
 
     private Double endTime = new Double(0);
     private Double currentTime = new Double(0);
@@ -35,58 +56,55 @@ public class playercontroller {
     private Media media = new Media(file.toURI().toString());
     private MediaPlayer mplayer = new MediaPlayer(media);
     FileChooser fileChooser = new FileChooser();
-    
-    public void start(Stage primarytStage){
-        
+
+    private WavFile wf;
+    private ArrayList<Double>[] signal_dB;
+
+    public void start(Stage primarytStage) {
+
         mView.fitWidthProperty().bind(pane.widthProperty());
         mView.fitHeightProperty().bind(pane.heightProperty());
 
-        mplayer.setOnEndOfMedia(() -> { 
+        mplayer.setOnEndOfMedia(() -> {
             mplayer.stop();
             btnPlay.setText("Play");
         });
-        
+
         fileChooser.setTitle("Open Media...");
-        fileChooser.getExtensionFilters().addAll(
-            new FileChooser.ExtensionFilter("MP4 Video", "*.mp4"),
-            new FileChooser.ExtensionFilter("MP3 Music", "*.mp3"),
-            new FileChooser.ExtensionFilter("WMV Music", "*.wmv"),
-            new FileChooser.ExtensionFilter("All Files", "*.*")
-            );
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("MP4 Video", "*.mp4"),
+                new FileChooser.ExtensionFilter("MP3 Music", "*.mp3"),
+                new FileChooser.ExtensionFilter("WMV Music", "*.wmv"),
+                new FileChooser.ExtensionFilter("All Files", "*.*"));
     }
 
-    int vol=50;
-    double speed=1;
-    public void initialize(){
-        slVolume.valueProperty().addListener(
-            new ChangeListener<Number>() {
-                @Override
-                public void changed(ObservableValue<? extends Number> ov,Number oldValue, Number newValue) {
-                    vol = newValue.intValue();
-                    lbVolume.setText(String.valueOf(vol));
-                }
-            }
-        );
+    int vol = 50;
+    double speed = 1;
 
-        slSpeed.valueProperty().addListener(
-            new ChangeListener<Number>() {
-                @Override
-                public void changed(ObservableValue<? extends Number> ov,Number oldValue, Number newValue) {
-                    speed = newValue.doubleValue();
-                    speed=Double.parseDouble(String.format("%.2f",speed));
-                    lbSpeed.setText(String.valueOf(speed));
-                }
+    public void initialize() {
+        slVolume.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> ov, Number oldValue, Number newValue) {
+                vol = newValue.intValue();
+                lbVolume.setText(String.valueOf(vol));
             }
-        );
-    }    
+        });
+
+        slSpeed.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> ov, Number oldValue, Number newValue) {
+                speed = newValue.doubleValue();
+                speed = Double.parseDouble(String.format("%.2f", speed));
+                lbSpeed.setText(String.valueOf(speed));
+            }
+        });
+    }
 
     @FXML
     void PlayClick(ActionEvent event) {
-        if (btnPlay.getText().equals("Play")){
+        if (btnPlay.getText().equals("Play")) {
             btnPlay.setText("Pause");
             mplayer.play();
-        }
-        else{
+        } else {
             btnPlay.setText("Play");
             mplayer.pause();
         }
@@ -99,10 +117,10 @@ public class playercontroller {
     }
 
     @FXML
-    void btnOpenClick(ActionEvent event) {
-        double sp=slSpeed.getValue();
-        file = fileChooser.showOpenDialog(new Stage()); 
-        if (file != null){
+    void btnOpenClick(ActionEvent event) throws IOException {
+        double sp = slSpeed.getValue();
+        file = fileChooser.showOpenDialog(new Stage());
+        if (file != null) {
             mplayer.stop();
             btnPlay.setText("Pause");
             media = new Media(file.toURI().toString());
@@ -110,43 +128,73 @@ public class playercontroller {
             mView.setMediaPlayer(mplayer);
             mplayer.setOnReady(() -> {
                 endTime = mplayer.getStopTime().toSeconds();
-            }); 
+            });
             mplayer.setOnEndOfMedia(() -> {
                 mplayer.stop();
                 mplayer.seek(Duration.ZERO);
                 btnPlay.setText("Play");
             });
-            mplayer.currentTimeProperty().addListener(ov->{
-            currentTime = mplayer.getCurrentTime().toSeconds();
-            lbCurrentTime.setText(Seconds2Str(currentTime)+"/"+Seconds2Str(endTime));
-            slTime.setValue(currentTime/endTime*100);
+            mplayer.currentTimeProperty().addListener(ov -> {
+                currentTime = mplayer.getCurrentTime().toSeconds();
+                lbCurrentTime.setText(Seconds2Str(currentTime) + "/" + Seconds2Str(endTime));
+                slTime.setValue(currentTime / endTime * 100);
             });
-            slTime.valueProperty().addListener(ov->{
-                if (slTime.isValueChanging()){
-                    mplayer.seek(mplayer.getTotalDuration().multiply(slTime.getValue()/100));
+            slTime.valueProperty().addListener(ov -> {
+                if (slTime.isValueChanging()) {
+                    mplayer.seek(mplayer.getTotalDuration().multiply(slTime.getValue() / 100));
                 }
             });
-            mplayer.volumeProperty().bind(slVolume.valueProperty().divide(100)); 
+            mplayer.volumeProperty().bind(slVolume.valueProperty().divide(100));
             mplayer.setRate(1);
-            slSpeed.valueProperty().addListener(ov->{
-                if (slSpeed.isValueChanging()){
+            slSpeed.valueProperty().addListener(ov -> {
+                if (slSpeed.isValueChanging()) {
                     mplayer.setRate(slSpeed.getValue());
                 }
             });
+
+            // draw waveform
+            drawWaveform();
             mplayer.play();
+
         }
     }
 
-    
-
-    private String Seconds2Str(Double seconds){
+    private String Seconds2Str(Double seconds) {
         Integer count = seconds.intValue();
         Integer Hours = count / 3600;
         count = count % 3600;
-        Integer Minutes = count /60;
+        Integer Minutes = count / 60;
         count = count % 60;
-        String str = Hours.toString()+":"+Minutes.toString()+":"+count.toString();
+        String str = Hours.toString() + ":" + Minutes.toString() + ":" + count.toString();
         return str;
+    }
+
+    private void drawWaveform() throws IOException {
+        GraphicsContext gc1 = canvas1.getGraphicsContext2D();
+        GraphicsContext gc2 = canvas2.getGraphicsContext2D();
+        wf = new WavFile();
+        wf.read(file.getAbsolutePath());
+        signal_dB = wf.getdBSignal();
+        int interval = signal_dB[0].size() / (int) canvas1.getWidth();
+        int max = 5;
+        int y_base = (int) canvas1.getHeight() / 2;
+        gc1.strokeLine(0, y_base, canvas1.getWidth(), y_base);
+        gc2.strokeLine(0, y_base, canvas2.getWidth(), y_base);
+        for (int x = 0; x < canvas1.getWidth(); x++) {
+            for (int channel = 0; channel < signal_dB.length; channel++) {
+                if (channel % 2 == 0) {
+                    gc1.strokeLine(x, y_base - (int) (signal_dB[channel].get(x * interval) * max), x + 1,
+                            y_base - (int) (signal_dB[channel].get((x + 1) * interval) * max));
+                    gc1.strokeLine(x, y_base + (int) (signal_dB[channel].get(x * interval) * max), x + 1,
+                            y_base + (int) (signal_dB[channel].get((x + 1) * interval) * max));
+                } else if (channel % 2 != 0) {
+                    gc2.strokeLine(x, y_base - (int) (signal_dB[channel].get(x * interval) * max), x + 1,
+                            y_base - (int) (signal_dB[channel].get((x + 1) * interval) * max));
+                    gc2.strokeLine(x, y_base + (int) (signal_dB[channel].get(x * interval) * max), x + 1,
+                            y_base + (int) (signal_dB[channel].get((x + 1) * interval) * max));
+                }
+            }
+        }
     }
 
 }
