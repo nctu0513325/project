826eb898b5dc19cc0a775c922c7b25706f551d23
fumchin <fumchin@gmail.com>
2020-06-28 03:56:47 +0800

    chord detection

--- FrequencyAnalysisController.java
+++ FrequencyAnalysisController.java
@@ -17,34 +17,47 @@ public class FrequencyAnalysisController extends FFTImplement {
     protected int index[][]; // WavFileeform
     private PlayerController PlayerController;
     private final double C_2 = 65.41;
-    private final double Csharp_2 = 69.30;
+    private final double Db_2 = 69.30;
     private final double D_2 = 73.42;
-    private final double Dsharp_2 = 77.78;
+    private final double Eb_2 = 77.78;
     private final double E_2 = 82.41;
     private final double F_2 = 87.31;
-    private final double Fsharp_2 = 92.50;
+    private final double Gb_2 = 92.50;
     private final double G_2 = 98;
-    private final double Gsharp_2 = 103.83;
+    private final double Ab_2 = 103.83;
     private final double A_2 = 110;
-    private final double Asharp_2 = 116.54;
+    private final double Bb_2 = 116.54;
     private final double B_2 = 123;
+    private final double C_3 = 2 * C_2;
+    private final double Db_3 = 2 * Db_2;
+    private final double D_3 = 2 * D_2;
+    private final double Eb_3 = 2 * Eb_2;
+    private final double E_3 = 2 * E_2;
+    private final double F_3 = 2 * F_2;
+    private final double Gb_3 = 2 * Gb_2;
+    private final double G_3 = 2 * G_2;
+    private final double Ab_3 = 2 * Ab_2;
+    private final double A_3 = 2 * A_2;
+    private final double Bb_3 = 2 * Bb_2;
+    private final double B_3 = 2 * B_2;
 
     private double C_sum = 0;
-    private double Csharp_sum = 0;
+    private double Db_sum = 0;
     private double D_sum = 0;
-    private double Dsharp_sum = 0;
+    private double Eb_sum = 0;
     private double E_sum = 0;
     private double F_sum = 0;
-    private double Fsharp_sum = 0;
+    private double Gb_sum = 0;
     private double G_sum = 0;
-    private double Gsharp_sum = 0;
+    private double Ab_sum = 0;
     private double A_sum = 0;
-    private double Asharp_sum = 0;
+    private double Bb_sum = 0;
     private double B_sum = 0;
+    private String rootString;
 
     // self constructor
     public FrequencyAnalysisController() {
-        super((int) Math.pow(2, 15));
+        super((int) Math.pow(2, 16));
     }
 
     // get signal form playerController
@@ -57,7 +70,7 @@ public class FrequencyAnalysisController extends FFTImplement {
                 signal_modify[channel] = new ArrayList(input[channel]);
             }
         } catch (NullPointerException e) {
-            System.out.println(e + "ss");
+            System.out.println(e);
         }
     }
 
@@ -65,7 +78,7 @@ public class FrequencyAnalysisController extends FFTImplement {
 
         part_signal_arr = new Complex[WavFile.getNumChannels()][sampleNum];
         fft_signal_arr = new Complex[WavFile.getNumChannels()][];
-        for (int count = 0; count < input[0].size() - sampleNum; count += sampleNum) {
+        for (int count = 0; count < input[0].size() - sampleNum; count += sampleNum / 3) {
             // initialize
             for (int col = 0; col < sampleNum; col++) {
                 for (int row = 0; row < WavFile.getNumChannels(); row++) {
@@ -94,7 +107,7 @@ public class FrequencyAnalysisController extends FFTImplement {
             Map<Integer, Double> linkedHashMap = new LinkedHashMap<>();
             int k = 0;
             for (Map.Entry e : list_Data) {
-                if (k == 5) {
+                if (k == 6) {
                     // System.out.println();
                     break;
                 }
@@ -108,19 +121,20 @@ public class FrequencyAnalysisController extends FFTImplement {
                 k++;
             }
             // make another map
+            rootString = getRoot(list_Data);
             getScalePower(list_Data);
             Map<String, Double> scaleMap = new HashMap<String, Double>();
             scaleMap.put("C", C_sum);
-            scaleMap.put("C#", Csharp_sum);
+            scaleMap.put("Db", Db_sum);
             scaleMap.put("D", D_sum);
-            scaleMap.put("D#", Dsharp_sum);
+            scaleMap.put("Eb", Eb_sum);
             scaleMap.put("E", E_sum);
             scaleMap.put("F", F_sum);
-            scaleMap.put("F#", Fsharp_sum);
+            scaleMap.put("Gb", Gb_sum);
             scaleMap.put("G", G_sum);
-            scaleMap.put("G#", Gsharp_sum);
+            scaleMap.put("Ab", Ab_sum);
             scaleMap.put("A", A_sum);
-            scaleMap.put("A#", Asharp_sum);
+            scaleMap.put("Bb", Bb_sum);
             scaleMap.put("B", B_sum);
             List<Map.Entry<String, Double>> list_scaleData = new ArrayList<Map.Entry<String, Double>>(
                     scaleMap.entrySet());
@@ -135,33 +149,38 @@ public class FrequencyAnalysisController extends FFTImplement {
             });
             k = 0;
             Map<String, Double> linkedScaleHashMap = new LinkedHashMap<>();
+            List<Map.Entry<String, Double>> sorted_list = new ArrayList<Map.Entry<String, Double>>();
             for (Map.Entry e : list_scaleData) {
-                if (k == 5) {
-                    // System.out.println();
+                if (k % 5 == 0) {
                     break;
                 }
+                // sorted_list.add(e);
                 linkedScaleHashMap.put((String) e.getKey(), (Double) e.getValue());
                 k++;
             }
-            System.out.println(linkedScaleHashMap);
+            // System.out.println(sorted_list.size());
+            for (int i = 0; i < 3; i++) {
+                list_scaleData.remove(list_scaleData.size() - 1);
+            }
+            String str = ChordComposition.findChord(list_scaleData, rootString);
 
-            // System.out.println(C_sum + "\t" + Csharp_sum + "\t" + D_sum + "\t" +
-            // Dsharp_sum + "\t" + E_sum + "\t"
-            // + F_sum + "\n" + Fsharp_sum + "\t" + G_sum + "\t" + Gsharp_sum + "\t" + A_sum
-            // + "\t" + Asharp_sum
+            // System.out.println(C_sum + "\t" + Db_sum + "\t" + D_sum + "\t" +
+            // Eb_sum + "\t" + E_sum + "\t"
+            // + F_sum + "\n" + Gb_sum + "\t" + G_sum + "\t" + Ab_sum + "\t" + A_sum
+            // + "\t" + Bb_sum
             // + "\t" + B_sum);
             System.out.println("============================================================================");
             C_sum = 0;
-            Csharp_sum = 0;
+            Db_sum = 0;
             D_sum = 0;
-            Dsharp_sum = 0;
+            Eb_sum = 0;
             E_sum = 0;
             F_sum = 0;
-            Fsharp_sum = 0;
+            Gb_sum = 0;
             G_sum = 0;
-            Gsharp_sum = 0;
+            Ab_sum = 0;
             A_sum = 0;
-            Asharp_sum = 0;
+            Bb_sum = 0;
             B_sum = 0;
         }
 
@@ -179,35 +198,128 @@ public class FrequencyAnalysisController extends FFTImplement {
             fre = (int) (index * WavFile.getSampleRate() / sampleNum);
             if ((fre % (int) Math.round(C_2)) <= range || (fre % (int) Math.round(C_2)) >= (C_2 - range)) {
                 C_sum += (Double) e.getValue();
-            } else if ((fre % (int) Math.round(Csharp_2)) <= range
-                    || (fre % (int) Math.round(Csharp_2)) >= (Csharp_2 - range)) {
-                Csharp_sum += (Double) e.getValue();
+            } else if ((fre % (int) Math.round(Db_2)) <= range || (fre % (int) Math.round(Db_2)) >= (Db_2 - range)) {
+                Db_sum += (Double) e.getValue();
             } else if ((fre % (int) Math.round(D_2)) <= range || (fre % (int) Math.round(D_2)) >= (D_2 - range)) {
                 D_sum += (Double) e.getValue();
-            } else if ((fre % (int) Math.round(Dsharp_2)) <= range
-                    || (fre % (int) Math.round(Dsharp_2)) >= (Dsharp_2 - range)) {
-                Dsharp_sum += (Double) e.getValue();
+            } else if ((fre % (int) Math.round(Eb_2)) <= range || (fre % (int) Math.round(Eb_2)) >= (Eb_2 - range)) {
+                Eb_sum += (Double) e.getValue();
             } else if ((fre % (int) Math.round(E_2)) <= range || (fre % (int) Math.round(E_2)) >= (E_2 - range)) {
                 E_sum += (Double) e.getValue();
             } else if ((fre % (int) Math.round(F_2)) <= range || (fre % (int) Math.round(F_2)) >= (F_2 - range)) {
                 F_sum += (Double) e.getValue();
-            } else if ((fre % (int) Math.round(Fsharp_2)) <= range
-                    || (fre % (int) Math.round(F_2)) >= (Fsharp_2 - range)) {
-                Fsharp_sum += (Double) e.getValue();
-            } else if ((fre % (int) Math.round(G_2)) <= range || (fre % (int) Math.round(F_2)) >= (G_2 - range)) {
+            } else if ((fre % (int) Math.round(Gb_2)) <= range || (fre % (int) Math.round(Gb_2)) >= (Gb_2 - range)) {
+                Gb_sum += (Double) e.getValue();
+            } else if ((fre % (int) Math.round(G_2)) <= range || (fre % (int) Math.round(G_2)) >= (G_2 - range)) {
                 G_sum += (Double) e.getValue();
-            } else if ((fre % (int) Math.round(Gsharp_2)) <= range
-                    || (fre % (int) Math.round(F_2)) >= (Gsharp_2 - range)) {
-                Gsharp_sum += (Double) e.getValue();
-            } else if ((fre % (int) Math.round(A_2)) <= range || (fre % (int) Math.round(F_2)) >= (A_2 - range)) {
+            } else if ((fre % (int) Math.round(Ab_2)) <= range || (fre % (int) Math.round(Ab_2)) >= (Ab_2 - range)) {
+                Ab_sum += (Double) e.getValue();
+            } else if ((fre % (int) Math.round(A_2)) <= range || (fre % (int) Math.round(A_2)) >= (A_2 - range)) {
                 A_sum += (Double) e.getValue();
-            } else if ((fre % (int) Math.round(Asharp_2)) <= range
-                    || (fre % (int) Math.round(F_2)) >= (Asharp_2 - range)) {
-                Asharp_sum += (Double) e.getValue();
-            } else if ((fre % (int) Math.round(B_2)) <= range || (fre % (int) Math.round(F_2)) >= (B_2 - range)) {
+            } else if ((fre % (int) Math.round(Bb_2)) <= range || (fre % (int) Math.round(Bb_2)) >= (Bb_2 - range)) {
+                Bb_sum += (Double) e.getValue();
+            } else if ((fre % (int) Math.round(B_2)) <= range || (fre % (int) Math.round(B_2)) >= (B_2 - range)) {
                 B_sum += (Double) e.getValue();
             }
+            // if ((fre % (int) Math.round(C_3)) <= range || (fre % (int) Math.round(C_3))
+            // >= (C_3 - range)) {
+            // C_sum += (Double) e.getValue();
+            // } else if ((fre % (int) Math.round(Db_3)) <= range || (fre % (int)
+            // Math.round(Db_3)) >= (Db_3 - range)) {
+            // Db_sum += (Double) e.getValue();
+            // } else if ((fre % (int) Math.round(D_3)) <= range || (fre % (int)
+            // Math.round(D_3)) >= (D_3 - range)) {
+            // D_sum += (Double) e.getValue();
+            // } else if ((fre % (int) Math.round(Eb_3)) <= range || (fre % (int)
+            // Math.round(Eb_3)) >= (Eb_3 - range)) {
+            // Eb_sum += (Double) e.getValue();
+            // } else if ((fre % (int) Math.round(E_3)) <= range || (fre % (int)
+            // Math.round(E_3)) >= (E_3 - range)) {
+            // E_sum += (Double) e.getValue();
+            // } else if ((fre % (int) Math.round(F_3)) <= range || (fre % (int)
+            // Math.round(F_3)) >= (F_3 - range)) {
+            // F_sum += (Double) e.getValue();
+            // } else if ((fre % (int) Math.round(Gb_3)) <= range || (fre % (int)
+            // Math.round(Gb_3)) >= (Gb_3 - range)) {
+            // Gb_sum += (Double) e.getValue();
+            // } else if ((fre % (int) Math.round(G_3)) <= range || (fre % (int)
+            // Math.round(G_3)) >= (G_3 - range)) {
+            // G_sum += (Double) e.getValue();
+            // } else if ((fre % (int) Math.round(Ab_3)) <= range || (fre % (int)
+            // Math.round(Ab_3)) >= (Ab_3 - range)) {
+            // Ab_sum += (Double) e.getValue();
+            // } else if ((fre % (int) Math.round(A_3)) <= range || (fre % (int)
+            // Math.round(A_3)) >= (A_3 - range)) {
+            // A_sum += (Double) e.getValue();
+            // } else if ((fre % (int) Math.round(Bb_3)) <= range || (fre % (int)
+            // Math.round(Bb_3)) >= (Bb_3 - range)) {
+            // Bb_sum += (Double) e.getValue();
+            // } else if ((fre % (int) Math.round(B_3)) <= range || (fre % (int)
+            // Math.round(B_3)) >= (B_3 - range)) {
+            // B_sum += (Double) e.getValue();
+            // }
         }
+    }
+
+    public String getRoot(List<Map.Entry<Integer, Double>> input) {
+        int index = 0;
+        int fre = 0;
+        int range = 3;
+        String result = "";
+        double max = 0;
+        double temp_power = 0;
+        String temp_root = "";
+        for (Map.Entry e : input) {
+            index = (int) e.getKey();
+            if (index >= sampleNum / range) {
+                index = sampleNum - index;
+            }
+            fre = (int) (index * WavFile.getSampleRate() / sampleNum);
+
+            if ((fre >= E_2 - range) && (fre <= E_2 + range)) {
+                temp_power = (Double) e.getValue();
+                temp_root = new String("E");
+            } else if ((fre >= F_2 - range) && (fre <= F_2 + range)) {
+                temp_power = (Double) e.getValue();
+                temp_root = new String("F");
+            } else if ((fre >= Gb_2 - range) && (fre <= Gb_2 + range)) {
+                temp_power = (Double) e.getValue();
+                temp_root = new String("Gb");
+            } else if ((fre >= G_2 - range) && (fre <= G_2 + range)) {
+                temp_power = (Double) e.getValue();
+                temp_root = new String("G");
+            } else if ((fre >= Ab_2 - range) && (fre <= Ab_2 + range)) {
+                temp_power = (Double) e.getValue();
+                temp_root = new String("Ab");
+            } else if ((fre >= A_2 - range) && (fre <= A_2 + range)) {
+                temp_power = (Double) e.getValue();
+                temp_root = new String("A");
+            } else if ((fre >= Bb_2 - range) && (fre <= Bb_2 + range)) {
+                temp_power = (Double) e.getValue();
+                temp_root = new String("Bb");
+            } else if ((fre >= C_3 - range) && (fre <= C_3 + range)) {
+                temp_power = (Double) e.getValue();
+                temp_root = new String("C");
+            } else if ((fre >= Db_3 - range) && (fre <= Db_3 + range)) {
+                temp_power = (Double) e.getValue();
+                temp_root = new String("Db");
+            } else if ((fre >= D_3 - range) && (fre <= D_3 + range)) {
+                temp_power = (Double) e.getValue();
+                temp_root = new String("D");
+            } else if ((fre >= Eb_3 - range) && (fre <= Eb_3 + range)) {
+                temp_power = (Double) e.getValue();
+                temp_root = new String("Eb");
+            } else if ((fre >= E_3 - range) && (fre <= E_3 + range)) {
+                temp_power = (Double) e.getValue();
+                temp_root = new String("E");
+            }
+
+            if (temp_power > max) {
+                max = temp_power;
+                result = temp_root;
+            }
+        }
+        return result;
     }
 
     @FXML
@@ -270,16 +382,16 @@ public class FrequencyAnalysisController extends FFTImplement {
         gc1.clearRect(0, 0, canvas1.getWidth(), canvas1.getHeight());
         /* f = 2*f for every octive */
         double C_2 = 65.41;
-        double Csharp_2 = 69.30;
+        double Db_2 = 69.30;
         double D_2s = 73.42;
-        double Dsharp_2 = 77.78;
+        double Eb_2 = 77.78;
         double E_2 = 82.41;
         double F_2 = 87.31;
-        double Fsharp_2 = 92.50;
+        double Gb_2 = 92.50;
         double G_2 = 98;
-        double Gsharp_2 = 103.83;
+        double Ab_2 = 103.83;
         double A_2 = 110;
-        double Asharp_2 = 116.54;
+        double Bb_2 = 116.54;
         double B_2 = 123;
 
         // double E_3 = 164;
